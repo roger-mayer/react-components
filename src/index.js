2da41af24364d3e8b9d60af9402da606c71e95b3
Roger Mayer <rmayer1984@gmail.com>
2020-03-22 10:52:41 -0500

    created approval card props

--- src/index.js
+++ src/index.js
@@ -3,30 +3,40 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
 import ApprovalCard from "./ApprovalCard";
+
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail
-                author="roger"
-                timeAgo="Today at 4:00PM"
-                content="Nice Blog Post"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="asher"
-                timeAgo="Today at 2:00PM"
-                content="Heyyyyy"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="riley"
-                timeAgo="Yesterday at 1:00PM"
-                content="What!???!?!?"
-                avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail
+                    author="roger"
+                    timeAgo="Today at 4:00PM"
+                    content="Nice Blog Post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="asher"
+                    timeAgo="Today at 2:00PM"
+                    content="Heyyyyy"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="riley"
+                    timeAgo="Yesterday at 1:00PM"
+                    content="What!???!?!?"
+                    avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
 
-    );
+    )
+        ;
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
+// test
